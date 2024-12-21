590b258d8d4f37caf89777b0f697b5957a64745f
Mohammad Elmahi <G00407950@atu.ie>
2024-12-21 20:56:57 +0000

    students loading

--- index.js
+++ index.js
@@ -3,25 +3,44 @@ const path = require('path');
 
 const app = express();
 
-
+// Set up EJS as the view engine
 app.set('view engine', 'ejs');
-
-
 app.set('views', path.join(__dirname, 'pages'));
 
-
-const studentsRoutes = require('./routes/students');
-const gradesRoutes = require('./routes/grades');
-const lecturersRoutes = require('./routes/lecturers');
-
-app.use('/students', studentsRoutes);
-app.use('/grades', gradesRoutes);
-app.use('/lecturers', lecturersRoutes);
+// Hardcoded students data
+const students = [
+    { sid: 'G001', name: 'New Name', age: 19 },
+    { sid: 'G002', name: 'Alison Conners', age: 23 },
+    { sid: 'G003', name: 'Thomas Murphy', age: 19 },
+    { sid: 'G004', name: 'Anne Greene', age: 23 },
+    { sid: 'G005', name: 'Tom Riddle', age: 27 },
+    { sid: 'G006', name: 'Brian Collins', age: 38 },
+    { sid: 'G007', name: 'Fiona O\'Hehir', age: 30 },
+    { sid: 'G008', name: 'George Johnson', age: 24 },
+    { sid: 'G009', name: 'Albert Newton', age: 31 },
+    { sid: 'G010', name: 'Marie Yeats', age: 21 },
+    { sid: 'G011', name: 'Jonathon Small', age: 22 },
+    { sid: 'G012', name: 'Barbara Harris', age: 23 },
+    { sid: 'G013', name: 'Oliver Flanagan', age: 19 },
+    { sid: 'G014', name: 'Neil Blaney', age: 34 },
+    { sid: 'G015', name: 'Nigel Delaney', age: 19 },
+    { sid: 'G016', name: 'Johnny Connors', age: 29 },
+    { sid: 'G017', name: 'Bill Turpin', age: 18 },
+    { sid: 'G018', name: 'Amanda Knox', age: 23 },
+    { sid: 'G019', name: 'James Joyce', age: 39 },
+    { sid: 'G020', name: 'Alice L\'Estrange', age: 32 },
+];
 
 
 app.get("/", (req, res) => {
     console.log("GET /");
     res.render("index", { title: "Home Page" });
+});
+
+
+app.get("/students", (req, res) => {
+    console.log("GET /students");
+    res.render("students", { title: "Students Page", students });
 });
 
 
--- routes/lecturers.js
+++ routes/lecturers.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const Lecturer = require('../models/Lecturer');
 
-router.get('/', (req, res) => {
-    res.render('lecturers', { title: 'Lecturers Page' });
+
+router.get('/', async (req, res) => {
+    try {
+        const lecturers = await Lecturer.find();
+        res.render('lecturers', { title: 'Lecturers Page', lecturers });
+    } catch (err) {
+        console.error('Error fetching lecturers:', err.message);
+        res.status(500).send('Error fetching lecturers.');
+    }
 });
 
 module.exports = router;
